67a14b34c957533fb6f5b5b25cddb5e921bbd791
Jodi Kidneigh <jodikidneigh@gmail.com>
2023-04-11 22:03:39 -0400

    Working version of count_primes.js

--- count_primes.js
+++ count_primes.js
@@ -1,25 +1,30 @@
 function countPrimes(number){
-    let prime_number_count = 0
-    let prime_numbers = []
-
-    for(let i = 2; i <= number; i++ ){
-        is_prime = true;
-
-        for (prime in prime_numbers){
-            if (i % prime == 0){
-                is_prime = false;
-                break;
-            }
-        if (is_prime = true){
-            prime_numbers.push(i);
+  
+    let prime_numbers = [2];
+  
+    if(number <= 1){
+      let prime_number_count = 0;
+    } else {
+      for(let i = 2; i < number; i++ ){
+        let is_prime = true;
+  
+        for (let prime in prime_numbers){
+          if (i % prime_numbers[prime] === 0){
+            is_prime = false;
+            break;
+          }
         }
-
+  
+        if (is_prime === true){
+          prime_numbers.push(i);
         }
-    } 
-    console.log(prime_numbers.toString)
-    prime_number_count = prime_numbers.length;
-
+  
+        console.log(i);
+      }
+    }
+    let prime_number_count = prime_numbers.length;
+  
     return prime_number_count;
-}
-
-console.log(countPrimes(18))
+  }
+  
+console.log(countPrimes(18));
